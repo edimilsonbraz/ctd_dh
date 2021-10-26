7f2383fa17a81fc0b952a712139a13d7ea508681
edimilsonbraz <edimilson.gt8@gmail.com>
2021-10-26 08:12:48 -0300

    update ex3.js ok

--- PROG_IMPERATIVA/ex3.js
+++ PROG_IMPERATIVA/ex3.js
@@ -10,11 +10,23 @@ let n2 = 8;
 console.log(n1 + n2);
 
 // 4) Atribua a soma das duas variáveis ​​anteriores a uma outra variável e exiba-a na tela.
+let soma = (n1 + n2)
+console.log(soma);
 
 // 5) Re-atribua à variável anterior "1" +1, e exiba na tela. (O que  aconteceu?)
- 
+ soma = "1" + 1;
+ console.log(soma)
+
 // 6) Re-atribua "olá" + 23, e exiba na tela. (O que aconteceu?)
+soma = "olá" + 23;
+console.log(soma)
 
 // 7) Crie mensagens concatenadas com valores diferentes.
+let name = "Edimilson";
+let idade = 18;
+let confirme = true;
+let nota = 10;
 
+console.log("O senhor " + name + " tem idade de " + idade + " anos: " + confirme)
+console.log("O Aluno " + name + " passou na Javascript com nota " + nota)
 
