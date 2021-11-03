a778457a796f2f10a1bdd036ce0c247637b339de
edimilsonbraz <edimilson.gt8@gmail.com>
2021-11-03 20:29:18 -0300

    exercicio Operadores Logicos aula07

---
+++ PROG_IMPERATIVA/aula07/operadores.js
@@ -0,0 +1,62 @@
+// Ex1. O que cada expressão retorna?
+// Dê uma olhada nestes exemplos e pense sobre o que cada um retorna:
+// 1. !true
+console.log(!true)
+
+// 2. !false
+console.log(!false)
+
+// 3. !!false
+console.log(!!false)
+
+// 4. !!true
+console.log(!!true)
+
+// 5. !1
+console.log(!1)
+
+// 6. !!1
+console.log(!!1)
+
+// 7. !0
+console.log(!0)
+
+// 8. !!0
+console.log(!!0)
+
+// 9. !!""
+console.log(!!"")
+
+// 10.
+let x = 5 ;
+let y = 9;
+x < 10 && x!==5
+console.log(x < 10 && x!==5)
+x >9 || x===5
+console.log(x >9 || x===5)
+!(x===y)
+console.log(!(x===y))
+
+// Ex2. O que cada expressão retorna?
+// Sem testar no console, o que você acha que cada uma destas expressões irá retornar?
+// Elas são true ou false?
+// 1. 
+let dx=10
+let dy ="a"
+dy==="b" || dx >= 10
+console.log(!!!0)
+
+// 2. 
+let gx=3
+let gy=8
+!(gx == "3" || gx === gy) && !(gy !== 8 && gx <= gy)
+console.log(!(gx == "3" || gx === gy) && !(gy !== 8 && gx <= gy))
+
+// 3. 
+let str = ""
+console.log("")
+
+let msg = "haha!"
+let eBonito = "false"
+!((str || msg) && eBonito)
+console.log(!((str || msg) && eBonito))
