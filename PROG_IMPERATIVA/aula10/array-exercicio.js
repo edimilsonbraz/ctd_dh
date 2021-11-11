e12cc6b2ce9865a8a3be17cc64dd7035c9efd770
edimilsonbraz <edimilson.gt8@gmail.com>
2021-11-10 21:16:27 -0300

    Aula 10 String e Array

---
+++ PROG_IMPERATIVA/aula10/array-exercicio.js
@@ -0,0 +1,21 @@
+let perifericos = ["memorias", "ssds", "processador", "fonte" ];
+
+
+console.log("O método Join faz junção")
+console.log(perifericos.join("*"),"\n")
+
+console.log("O método Push adiciona elementos ao final do array")
+console.log(perifericos.push("Monitor"),"")
+console.log(perifericos, "\n")
+
+console.log("O método Pop elimina o ultimo elemento do array")
+console.log(perifericos.pop("Monitor"))
+console.log(perifericos, "\n")
+
+console.log("O método Shift elimina o primeiro elemento do array")
+console.log(perifericos.shift())
+console.log(perifericos, "\n")
+
+console.log("O método Unshift adiciona elementos ao inicio do array")
+console.log(perifericos.unshift("Gabinete", "Teclado"))
+console.log(perifericos, "\n")
