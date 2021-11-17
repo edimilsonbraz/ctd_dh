60208d9d75cd40b38842d326d391f6e369a2fe38
edimilsonbraz <edimilson.gt8@gmail.com>
2021-11-16 21:20:32 -0300

    create aula 11 Estrutura de repeticao

---
+++ PROG_IMPERATIVA/aula11/estruturaRepeticao.js
@@ -0,0 +1,29 @@
+// ESTRUTURA DE REPETIÇÃO FOR
+
+for(let contador = 1; contador <= 5; contador++) {
+  console.log("Olá mundo" + " ", contador)
+}
+
+console.log("======================")
+
+// ESTRUTURA DE REPETIÇÃO WHILE 
+let cont = 0;
+while(cont <= 5) {
+  console.log(cont)
+  cont++;
+}
+console.log(cont)
+
+console.log("======================")
+
+
+// ESTRUTURA DE REPETIÇÃO
+for(let i = 0; i <= 10; i= i+2) {
+  console.log("Contador == ", i)
+}
+
+// ESTRUTURA DE REPETI
+let nomes = ["Marcos", "Fulano", "Ciclano", "Edimilson"];
+for(let i =0; i < nomes.length; i++) {
+  console.log(nomes[i])
+}
