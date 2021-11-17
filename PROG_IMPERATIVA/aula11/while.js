fe2d62179b560ef7f85c4b08518aa2eb46cff347
edimilsonbraz <edimilson.gt8@gmail.com>
2021-11-17 17:06:36 -0300

    exercicios while

---
+++ PROG_IMPERATIVA/aula11/while.js
@@ -0,0 +1,30 @@
+// 1. Programa em Javascript que mostra os números ímpares
+// Escreva um aplicativo em Javascript que mostre todos os 
+// números ímpares de 1 até 100.
+console.log("NUMEROS IMPARES DE 1 Á 100")
+let numbers = 1;
+while(numbers <=100) {
+  console.log(numbers);
+  numbers = numbers + 2;
+}
+
+console.log("==============================")
+console.log("NUMEROS PARES DE 1 Á 100")
+// 2. Programa em Javascript que mostra os números pares
+// Escreva um aplicativo em Javascript que mostre todos 
+// os números pares de 1 até 100.
+let num = 0;
+while (num <= 100) {
+  console.log(num);
+  num = num + 2;
+}
+
+
+console.log("==============================")
+console.log("NUMEROS INTEIROS")
+// 3. Programa em Javascript que mostra os números pares e ímpares 
+// Escreva um aplicativo em Javascript que recebe um número inteiro e 
+// mostra os números pares e ímpares (separados), de 1 até esse inteiro.
+
+
+// 4. Programa em Javascript que calcula a média das notas de uma turma
