bfeaee9a693bebe605957173dc0f9d143a712876
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-24 16:58:18 -0300

    :white_check_mark: Update main.js Manip a DOM

---
+++ FRONT-END/segundo-bimestre/AULA04/main.js
@@ -0,0 +1,32 @@
+const body = document.querySelector('body');
+console.log(body)
+
+const h1 = document.querySelector('h1');
+console.log(h1)
+
+const conteudo = document.querySelector('.conteudo');
+console.log(conteudo)
+
+const h5 = document.querySelector('h5');
+console.log(h5)
+
+const list = document.querySelector('ul');
+console.log(list)
+
+const item = document.getElementById('item1');
+console.log(item)
+
+const section = document.querySelector('section');
+console.log(section)
+
+const card1 = document.getElementById('card1');
+console.log(card1)
+
+const cardAnimado = document.querySelector('.animado');
+console.log(cardAnimado)
+
+const artigos = document.querySelectorAll('article');
+console.log(artigos)
+
+
+
