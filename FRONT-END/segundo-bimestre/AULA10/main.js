6f7b0f36db8853279e08047b1117b05c6e3374e1
edimilsonbraz <edimilson.gt8@gmail.com>
2022-03-21 19:07:47 -0300

    :zap: update aula 10
    update aula 10

--- FRONT-END/segundo-bimestre/AULA10/main.js
+++ FRONT-END/segundo-bimestre/AULA10/main.js
@@ -7,24 +7,25 @@ submit.addEventListener('click', (event) => {
   
   event.preventDefault()
 
-  let name = document.createElement('p')
+  let createTagP1 = document.createElement('p')
   let textName = document.createTextNode(firstName)
-  name.appendChild(textName)
+  createTagP1.appendChild(textName)
+  
 
-  let surName = document.createElement('p')
-  let textSurName= document.createTextNode(LastName)
-  surName.appendChild(textSurName)
+  let createTagP2 = document.createElement('p')
+  let textLastName= document.createTextNode(LastName)
+  createTagP2.appendChild(textLastName)
 
 
-  const fullName = name.concat(" ", surName);
-  console.log(fullName)
+  // const fullName = name.concat(" ", surName);
+  // console.log(fullName)
   // let totalName = document.createElement('p')
   // let textFullName = document.createTextNode(fullName)
   // totalName.appendChild(textFullName)
 
   //Setando na Div
-  content.appendChild(name)
-  content.appendChild(surName)
+  content.appendChild(createTagP1)
+  content.appendChild(createTagP2)
   // content.appendChild(totalName)
   
 })
