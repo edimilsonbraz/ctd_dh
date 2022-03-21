1f46b2fb5ae0115e5f69c0303d06d1c8f893fd89
edimilsonbraz <edimilson.gt8@gmail.com>
2022-03-21 18:23:52 -0300

    :art: aula 10

--- FRONT-END/segundo-bimestre/AULA10/main.js
+++ FRONT-END/segundo-bimestre/AULA10/main.js
@@ -1,3 +1,31 @@
-const inputValue = document.getElementById('text').value;
+const submit = document.querySelector('button');
 
-console.log(inputValue);
+submit.addEventListener('click', (event) => {
+  const firstName = document.getElementById('name').value;
+  const LastName = document.getElementById('lastname').value;
+  const content = document.querySelector('form');
+  
+  event.preventDefault()
+
+  let name = document.createElement('p')
+  let textName = document.createTextNode(firstName)
+  name.appendChild(textName)
+
+  let surName = document.createElement('p')
+  let textSurName= document.createTextNode(LastName)
+  surName.appendChild(textSurName)
+
+
+  const fullName = name.concat(" ", surName);
+  console.log(fullName)
+  // let totalName = document.createElement('p')
+  // let textFullName = document.createTextNode(fullName)
+  // totalName.appendChild(textFullName)
+
+  //Setando na Div
+  content.appendChild(name)
+  content.appendChild(surName)
+  // content.appendChild(totalName)
+  
+})
+
