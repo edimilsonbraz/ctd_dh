3e03d2be4ed71b6a791bff380df96efd6e3dd615
edimilsonbraz <edimilson.gt8@gmail.com>
2022-03-24 16:24:10 -0300

    :white_check_mark: update sessionStorage

--- FRONT-END/segundo-bimestre/AULA14/PRATICA_LocalStorageA/scripts/index.js
+++ FRONT-END/segundo-bimestre/AULA14/PRATICA_LocalStorageA/scripts/index.js
@@ -27,6 +27,8 @@ const baseDeDados = {
     },
   ],
 };
+
+
 //Emails do usuario separados em um array
 const emailsUsuarios = [];
 baseDeDados.usuarios.forEach(item => {
@@ -91,43 +93,37 @@ botaoSessao.addEventListener('click', () => {
     statusError.classList.remove('hidden')
   }
 
-  if(sessionStorage.getItem("email") == emailDoBodyInput) {
-    bemVindo()
+  
+})
+
+//Persistindo os Dados
+window.onload = () => {
+  sessionUsuario()
+}
+
+
+function sessionUsuario() {
+  let objetoUsuarioCapturado = sessionStorage.getItem("email")
+  if(objetoUsuarioCapturado != null) {
+    const form = document.querySelector('form');
+    const title = document.getElementById('title');
+    const sessaoUsuario = document.getElementById('bem-vindo');
+    const titleSessao = document.getElementById('title-sessao');
+
+    form.classList.add('hidden');
+    title.classList.add('hidden');
+    sessaoUsuario.classList.remove('hidden');
+    titleSessao.innerText = `Bem-vindo ao site 🤓🖖`
   }else{
     console.log("Algo errado")
   }
-   
-})
+}
+ const botaoFinishSession = document.getElementById('btn-sessao')
+ botaoFinishSession.addEventListener('click', finisheSession)
+
+function finisheSession() {
+  sessionStorage.clear();
+  document.location.reload(true);
+}
 
 
-//// ATIVIDADE
-
-/// Passo a passo:
-
-// 1) Quando a pessoa faz o login, se as validações que já temos no local tiverem sido bem sucedidas,
-// devemos armazenar a informação do utilizador no LocalStorage.OK
-
-// 2) Na mensagem de boas-vindas que já tínhamos implementado, teremos de acrescentar
-// o nome da pessoa e um botão "Logout". OK
-
-// 3) Uma vez iniciada a sessão, a sessão deve ser mantida nesse estado no caso de a pessoa recarregar a página. 
-// Para fazer isso, terá de validar se a informação do utilizador existe neste momento em
-// quando a página é carregada, e com base nessa condição decidir quais os elementos mostrar.
-
-// 4) No caso de a pessoa clicar no botão "Logout", deverá ser eliminada
-// a informação do utilizador, exibir uma mensagem indicando que a sessão foi encerrada, e recarregar
-// a página para voltar a exibir o formulário de login.
-
-/* 
-DICAS:
-  - Para alcançar os objetivos deste exercício, você terá de recorrer a alguns dos eventos e métodos que vimos em aulas anteriores.
-    Te convidamos a verificar esses recursos caso tenha alguma dúvida, pois neles encontrará todas as respostas  necessárias para completar a actividade.
-
-  - Lembre-se que pode selecionar e manipular os elementos do arquivo index.html, utilizando os recursos que o Javascript lhe oferece. Além disso, no arquivo styles.css tem algumas classes predefinidas e estilos pré-definidos para o ajudar a completar a actividade.
-
-  - Ao guardar informação do utilizador no browser, lembre-se que devemos guardar apenas a informação necessária, e NÃO DEVEMOS GUARDAR A SENHA. 
-    Desta maneira, você deve selecionar e separar as informações que tem de armazenar, a partir do objeto que contém a informação do utilizador. 
-
-   Mão à obra! Bora pro código!
-
- */
