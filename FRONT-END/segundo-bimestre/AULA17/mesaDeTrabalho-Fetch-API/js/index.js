f01e4578a2ae16a8fcf5469c7bfb8554abf0006d
edimilsonbraz <edimilson.gt8@gmail.com>
2022-03-31 08:42:29 -0300

    :white_check_mark: Update scripts APIs

--- FRONT-END/segundo-bimestre/AULA17/mesaDeTrabalho-Fetch-API/js/index.js
+++ FRONT-END/segundo-bimestre/AULA17/mesaDeTrabalho-Fetch-API/js/index.js
@@ -1,38 +1,38 @@
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
 async function getImagesApi() {
-    await fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        renderizarDadosUsuario(data)
-    })
-    .catch(error => {
-        console.log(error)
-    })
+  await fetch('https://randomuser.me/api/')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    //manipulamos a resposta
+    renderizarDadosUsuario(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 }
     
 function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    let urlImage = dados.results[0].picture.large
-    let userName = `${dados.results[0].name.first} ${dados.results[0].name.last}`
-    let userEmail = dados.results[0].email
+  /* -------------------------------- Tarefa 1 -------------------------------- */
+  let urlImage = dados.results[0].picture.large
+  let userName = `${dados.results[0].name.first} ${dados.results[0].name.last}`
+  let userEmail = dados.results[0].email
 
-    const card = document.querySelector('.card');
+  const card = document.querySelector('.card');
 
-    card.innerHTML = `
-    <img src="${urlImage}">
-    <div>
-        <h2>${userName}</h2>
-        <p>${userEmail}</p>
-    </div>
-    `
+  card.innerHTML = `
+  <img src="${urlImage}">
+  <div>
+    <h2>${userName}</h2>
+    <p>${userEmail}</p>
+  </div>
+  `
 }
 
 /* --------------------------- Tarefa 2 (extra) --------------------------- */
 
 const btnRandom = document.getElementById('random')
 btnRandom.addEventListener('click', () => {
-    getImagesApi()
+  getImagesApi()
 })
