410c5112c4c4978fb4b8b9ab6362c734330b3dc5
edimilsonbraz <edimilson.gt8@gmail.com>
2022-06-07 20:52:37 -0300

    :sparkles: Introdu

---
+++ JAVA/Aula_04/src/Main.java
@@ -0,0 +1,26 @@
+class Main {
+    public static void main(String[] args) {
+
+        String nome1 = "Shesheu";
+        String nome2 = "Nemo";
+        String nome3 = "Snow";
+        int idade = 2;
+        int idade2 = 1;
+        int idade3 = 3;
+        double comida = 1.5;
+        double comida2 = 0.14;
+        double comida3 = 0.5;
+        String som = "au au";
+        String som2 = "glub glub";
+        String som3 = "Miau Miau";
+        System.out.println( nome1 + " tem " + idade +" anos");
+        System.out.println( nome1 + " come " + comida +" kg de ração e faz " + som );
+
+        System.out.println( nome2 + " tem " + idade2 +" ano");
+        System.out.println( nome2 + " come  " + comida2 +" gramas e faz " + som2 );
+
+        System.out.println( nome3 + " tem " + idade3 +" anos");
+        System.out.println( nome3 + " come  " + comida3 +" kg e faz " + som3 );
+
+    }
+}
