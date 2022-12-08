952a2c75c1c507d1584b98f31148dc255931dc72
edimilson braz <edimilson.gt8@gmail.com>
2022-12-08 09:18:40 -0300

    :sparkles: Criacao de rotas / reestruturacao

--- FRONT-END/terceiro-bimestre/checkpoint-2/src/App.jsx
+++ FRONT-END/terceiro-bimestre/checkpoint-2/src/App.jsx
@@ -1,22 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Card from './Components/Card'
+import { Contato } from './pages/Contato'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+import Navbar from './Components/Navbar.jsx'
+import Footer from './Components/Footer'
 
-import { Outlet } from "react-router-dom";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
-function App() {
+export function App() {
   return (
-    <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar a classe dark ou light */}
-      <div className={`app light}`}>
-        <Navbar />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </>
-  );
+    // <>
+    //   {/* //Na linha seguinte deverá ser feito um teste se a aplicação
+    //     // está em dark mode e deverá utilizar a classe dark ou light */}
+    //   <div className={`app light}`}>
+    //     <Navbar />
+    //     <main>
+    //       <Outlet />
+    //     </main>
+    //     <Footer />
+    //   </div>
+    // </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dentist/:id" element={<Detail />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/destacados" element={<Card />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
-
-export default App;
